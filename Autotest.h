2815232ad6539a65f2445e3ec0d8edd72dd62bbc
Alex <nilbak314@gmail.com>
2020-10-08 16:44:56 +0300

    meet the autotest!

---
+++ Autotest.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "CPoint.h"
+#include "CDist.h"
+using namespace std;
+
+int Autotest1(){
+    CPoint A(0,1);
+    CDist B(1,2);
+    CPoint S=A+B;
+    if (S.X()==1 && S.Y()==3){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+int Autotest2(){
+    CDist A(-1,0);
+    CPoint B(1,2);
+    CPoint S=A+B;
+    if (S.X()==0 && S.Y()==2){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+int Autotest3(){
+    CPoint A(2,1);
+    CPoint B(1,2);
+    CPoint S=A-B;
+    if (S.X()==1 && S.Y()==-1){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,6 @@
 /*
 12. ќпределить классы CPoint дл€ работы с целочисленными точками на плоскости и CDist дл€ работы с рассто€нием между точками.
-¬ классе должны быть определены необходимые конструкторы, операторы сложени€(CPoint и CDist, возвращающий CPoint, 
+¬ классе должны быть определены необходимые конструкторы, операторы сложени€(CPoint и CDist, возвращающий CPoint,
 а также CDist и CPoint, возвращающий CPoint), вычитани€(CPoint из CPoint, возвращающий CDist).
 ¬ отдельном файле должен быть написан тест на данный класс.
 */
@@ -9,12 +9,30 @@
 #include <fstream>
 #include "CPoint.h"
 #include "CDist.h"
+#include "Autotest.h"
 using namespace std;
 
 int main(void) {
+    int k;
+    k=Autotest1();
+    if(k!=0){
+        cout << "Problem with operator+(CPoint)\n" << endl;
+        return 1;
+    }
+    k=Autotest2();
+    if(k!=0){
+        cout << "Problem with operator+(CDist)\n" << endl;
+        return 1;
+    }
+    k=Autotest3();
+    if(k!=0){
+        cout << "Problem with operator-(CPoint)\n" << endl;
+        return 1;
+    }
+
     ifstream input("data.txt");
     if (!input.is_open()) {
-        cout << "Cannot open 'data.txt'\n";
+        cout << "Cannot open 'data.txt'\n" << endl;
         return 1;
     }
     int a1, a2, b1, b2, c1, c2;
